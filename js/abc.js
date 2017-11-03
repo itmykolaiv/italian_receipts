ba64ae11f556fd1c3e118624ffcad3612a7a9159
AllaSmyshlyaeva <alla.smyshlyaeva@gmail.com>
2017-11-03 12:20:30 +0200

    add new functions

--- js/abc.js
+++ js/abc.js
@@ -1,14 +1,21 @@
 var baseUrl = 'http://ec2-52-14-195-100.us-east-2.compute.amazonaws.com/italian_recipes';
 function send_request(command, query){
-    fetch(config.baseUrl + '/' +command+'/search?q=pizza&key=6f71266a81069a9eb5103033f9efabc2')
+  fetch(baseUrl + '/' +command+'/?key=6f71266a81069a9eb5103033f9efabc2' + query)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(user) {
+      console.log(data);
+    })
+    .catch(function () {
 
- .then(function(response) {
-
-    return response.json();
-   })
-  .then(function(user) {
-   console.log(data);
-  })
-  .catch( alert );
+    });
 }
-send_request();
+function build_recipes() {
+  var rep_container = document.querySelector('.js_recipes-container');
+}
+
+function create_recipe() {
+  var rep_div = document.createElement('div');
+  //Create another elements with needed attributes.
+}
