691eb77450a2db31e8772e77830f729c6c77e1a1
Denis Bilchenko <dbilchenko@geeksforless.net>
2017-11-10 13:41:14 +0200

    add dynamic recipies

--- js/abc.js
+++ js/abc.js
@@ -1,9 +1,9 @@
 function send_request(command, query, cb) {
-  var baseUrl = 'http://ec2-52-14-195-100.us-east-2.compute.amazonaws.com/italian_recipes';
+  var baseUrl = 'https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes';
   var myHeaders = new Headers();
   myHeaders.append("X-Mashape-Key", "zd1IXUWYTnmshVbob13BKiSwWEKdp1PfBb3jsnC4LZHFgUehE1");
 
-  fetch(baseUrl + '/' + command + '/?' + query, {headers: myHeaders})
+  fetch(baseUrl + '/' + command + '/' + query, {headers: myHeaders})
       .then(function(response) {
           return response.json();
       })
@@ -15,8 +15,34 @@ function send_request(command, query, cb) {
 
 
 function build_recipes() {
-  var rep_container = document.querySelector('.js_recipes-container');
-  
+  var templ = `
+  <div class="one-recipe clear">
+    <div class="one-recipe-descr">
+      <h2>
+        <a href="vievrecipe.html?id={id}">{title}</a>
+      </h2>
+      <p>{description}</p>
+      <a href="vievrecipe.html?id={id}" class="view">view all</a>
+    </div>
+    <div class="one-recipe-image">
+      <a href="vievrecipe.html?id={id}">
+        <img src="{image_link}">
+      </a>
+    </div>
+  </div>`;
+  send_request('random', '?number=10&tags=italian', function (data) {
+    for (var i = 0; i < data.recipes.length; i++) {
+      var content = templ.replace(/\{id\}/g, data.recipes[i].id).replace('{title}', data.recipes[i].title).replace('{image_link}', data.recipes[i].image);
+      get_summary(content, data.recipes[i].id);
+    }
+  });
+}
+function get_summary(content, id) {
+  send_request(id + '/summary', '', function(data) {
+    content = content.replace('{description}', data.summary);
+    var wrapper = document.querySelector('.js_recipes-wrapper');
+    wrapper.innerHTML += content;
+  });
 }
 
 function create_recipe() {
--- js/custom.js
+++ js/custom.js
@@ -1,24 +1,32 @@
 document.addEventListener("DOMContentLoaded", function() {
-    send_request('recipes/random', 'limitLicense=false&number=6&tags=italian', function(data) {
-        var slider = document.querySelector('.js_slider');
-        for(var i = 0; i < data.recipes.length; i++) {
-            var img = document.createElement('img');
-            var div = document.createElement('div');
-            img.src = data.recipes[i].image;
-            div.append(img);
-            slider.append(div);
-        }
-        var slider = tns({
-            "container": ".js_slider",
-            "items": 3,
-            "speed": 300,
-            "autoplay": true,
-            "autoplayHoverPause": true,
-            "autoplayTimeout": 2500,
-            "autoplayText": [
-                "▶",
-                "❚❚"
-            ]
+    var slider = document.querySelector('.js_slider');
+    if (slider) {
+        send_request('recipes/random', '?limitLicense=false&number=6&tags=italian', function(data) {
+            for(var i = 0; i < data.recipes.length; i++) {
+                var img = document.createElement('img');
+                var div = document.createElement('div');
+                img.src = data.recipes[i].image;
+                div.append(img);
+                slider.append(div);
+            }
+            var slider = tns({
+                "container": ".js_slider",
+                "items": 3,
+                "speed": 300,
+                "autoplay": true,
+                "autoplayHoverPause": true,
+                "autoplayTimeout": 2500,
+                "autoplayText": [
+                    "▶",
+                    "❚❚"
+                ]
+            });
         });
-    });
+    }
+    var wrapper = document.querySelector('.js_recipes-wrapper');
+    if (wrapper) {
+        wrapper.innerHTML = '';
+        build_recipes();
+    }
+    
 });
